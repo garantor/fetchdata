f416c183ef41dc041bc0716826c4e1e4e54f50d6
garantor <sundayafolabi992@gmail.com>
2022-02-23 12:46:17 +0100

    initial commit, jsonplacholder not loading

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Form from "./Components/Form"
+import { useState, useEffect } from 'react'
 function App() {
+  const API_URL = 'https://jsonplaceholder.typicode.com/';
+  const [reqType, setReqType] = useState('get')
+  const [Items, setItems] = useState()
+
+  useEffect(
+    () => {
+     async function fetchItems (){
+      try {
+        console.log(`${API_URL}${reqType}`);
+        const response = await fetch(`${API_URL}${reqType}`);
+        const data = await response.json();
+        setItems(data);
+        console.log(data)
+      } catch (err) {
+        console.log(err);
+      }
+    }
+
+    fetchItems();
+
+  }, [reqType])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <Form reqType={reqType} setReqType={setReqType} />
     </div>
   );
 }
